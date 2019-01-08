587024b994548205e5b74ed9be75a10a8b7c7404
ru-ni <mark@thezu.cc>
2019-01-08 12:17:19 +0000

    util funcs added
    I'm fairly sure I wont need to re-write these so I don't mind just ripping them from my last version.
    
    Next commit should be the refactored or rewritten bank functions

--- serpo.go
+++ serpo.go
@@ -67,4 +67,5 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	// If the message is "pong" reply with "Ping!"
 	if m.Content == "pong" {
 		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
 }
---
+++ util.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+func Contains(a []string, x string) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
+}
+func random(min int, max int) int {
+	return rand.Intn(max-min) + min
+}
+func randbool() bool {
+	return rand.Float32() < 0.5
+}
+
+func ranum(max int) int {
+	s1 := rand.NewSource(time.Now().UnixNano())
+	time.Sleep(20 * time.Millisecond)
+	r1 := rand.New(s1)
+	if max == 0 {
+		max = 1
+	}
+	return r1.Intn(max)
+}
+
+// isValidUrl tests a string to determine if it is a url or not.
+func isValidUrl(toTest string) bool {
+	_, err := url.ParseRequestURI(toTest)
+	if err != nil {
+		return false
+	} else {
+		return true
+	}
+}
+
+func rankByWordCount(wordFrequencies map[string]int) PairList {
+	pl := make(PairList, len(wordFrequencies))
+	i := 0
+	for k, v := range wordFrequencies {
+		pl[i] = Pair{k, v}
+		i++
+	}
+	sort.Sort(sort.Reverse(pl))
+	return pl
+}
+
+type Pair struct {
+	Key   string
+	Value int
+}
+
+type PairList []Pair
+
+func (p PairList) Len() int           { return len(p) }
+func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
+func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
